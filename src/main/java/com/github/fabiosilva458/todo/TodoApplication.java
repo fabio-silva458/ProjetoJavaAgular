49865cac13be69b30404c077922ce57debf641cc
fabio-silva458 <fabiomyssionhills@gmail.com>
2022-07-30 16:37:19 -0300

    Projeto iniciado

--- src/main/java/com/github/fabiosilva458/todo/TodoApplication.java
+++ src/main/java/com/github/fabiosilva458/todo/TodoApplication.java
@@ -2,10 +2,19 @@ package com.github.fabiosilva458.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodoApplication {
-
+	 @GetMapping("/")
+	public String Hello() {
+		
+		return "Helou Word";
+	}
+	
+    
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}

11dfd558406a3dffd1b91a768ab7c7f7819a4fb6
fabio-silva458 <fabiomyssionhills@gmail.com>
2022-07-30 19:09:28 -0300

    Manipulado Dados

---
+++ src/main/java/com/github/fabiosilva458/todo/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.github.fabiosilva458.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.fabiosilva458.todo.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>  {
+
+	
+	
+	
+
+}
